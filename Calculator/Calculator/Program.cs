935d296744aff252e09c57a10efdd3a4e1ac6966
Einar Rocha <einar_21@hotmail.com>
2018-09-17 23:44:31 -0400

    Little example

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -10,6 +10,11 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Number a = new Number(1);
+            Number b = new Number(2);
+
+            Calculator calculator = new Calculator();
+            calculator.Addition(a,b);
         }
     }
 }
